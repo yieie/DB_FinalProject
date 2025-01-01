61b15935501cf57680fe8e8c6ed128534265be84
yieie <ison632413672@gmail.com>
2025-01-01 16:02:09 +0800

    新增Service註解

--- frontend/lib/admin/logic/AdminTeamService.dart
+++ frontend/lib/admin/logic/AdminTeamService.dart
@@ -1,10 +1,12 @@
-import 'package:db_finalproject/common/data/TeamRepository.dart';
+import 'package:db_finalproject/core/services/ApiService.dart';
 import 'package:db_finalproject/data/Team.dart';
+import 'dart:convert';
 
 class AdminTeamService {
-  final TeamRepository _teamRepository = TeamRepository();
+  final ApiService _apiService = ApiService();
 
-  Future<List<Team>> getBasicAllTeam(){
-    return _teamRepository.fetchBasicAllTeam();
+  Future<List<Team>> getBasicAllTeam() async{
+    final response = await _apiService.get('Team/Cond');
+    return (jsonDecode(response.body) as List).map((json) => Team.fromBasicJson(json)).toList();
   }
 }
--- frontend/lib/admin/logic/TeamsStatusService.dart
+++ frontend/lib/admin/logic/TeamsStatusService.dart
@@ -3,6 +3,7 @@ import 'package:db_finalproject/core/services/ApiService.dart';
 class TeamsStatusService {
   final ApiService _apiService = ApiService();
 
+  //拿所有隊伍狀態，已報名、待審核、已審核、初賽隊伍、決賽隊伍的隊伍數
   Future<TeamsStatus> getTeamsStatus() async{
     final response = await _apiService.get('/Teams/Status');
     return TeamsStatus.fromJson(response);
