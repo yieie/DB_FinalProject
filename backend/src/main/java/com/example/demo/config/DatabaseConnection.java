02a5aa22db771527f329f9ce2bd064e2ab841986
Guanisnt <allen6010101@gmail.com>
2025-01-06 03:48:03 +0800

    merge zhen

--- backend/src/main/java/com/example/demo/config/DatabaseConnection.java
+++ backend/src/main/java/com/example/demo/config/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/dbfinalproject";
     // private static final String URL = "jdbc:mariadb://localhost:3306/dbfinalproject";
     private static final String USER = "root";
-    private static final String PASSWORD = "a1115522";
+    private static final String PASSWORD = "12345678";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
